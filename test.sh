7df84e9a56a523fd13bca063afedc99ed2f6af6f
Roberto Cunha <robertosilvino@gmail.com>
2018-10-28 02:47:05 -0300

    stages
    stages conditional
    
    git tag
    
    git tag2
    
    tag 3
    
    variable test
    
    tag3 changed
    
    tag4
    
    tag5
    
    tag 6
    
    tag7
    
    tag8
    
    tag9
    
    tag10

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ "development" =~ ^development|staging|production$ ]]; then echo "OK"; fi
+export TRAVIS_BRANCH=v1.0_tcc_staging
+export REGEX_PUSH_BRANCH='staging|production'
+export REGEX_DEPLOY_BRANCH='staging|production'
+
+if [[ ${TRAVIS_BRANCH} =~ ${REGEX_DEPLOY_BRANCH} ]]; then echo "Deploy OK"; fi
+if [[ ${TRAVIS_BRANCH} =~ ${REGEX_PUSH_BRANCH} ]]; then echo "Push OK"; fi
+
+# travis ci changed
